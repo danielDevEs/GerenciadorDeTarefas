fb7c5b673d9b180c0263362463f901433fcf44ed
Daniel <danielandrade07@alu.ufc.br>
2026-06-01 14:11:20 -0300

    Conflito

--- gerenciador-tarefas/src/main/java/br/com/lip/gerenciador_tarefas/controller/UsuarioController.java
+++ gerenciador-tarefas/src/main/java/br/com/lip/gerenciador_tarefas/controller/UsuarioController.java
@@ -1,29 +1,9 @@
 package br.com.lip.gerenciador_tarefas.controller;
 
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.lip.gerenciador_tarefas.model.Usuario;
-
-
-
-@RestController
-public class UsuarioController {
-    @GetMapping("/usuario")
-    public String AdicionarUsuario() {
-        
-        Usuario usuario = new Usuario();
-
-        usuario.setId("Daniel");
-        usuario.setSenha(1201921);
-
-        return usuario;
-    }
-    
-=======
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
@@ -33,5 +13,4 @@ public class UsuarioController {
         return "Maria Camila";
     }
 
->>>>>>> refs/remotes/origin/main
 }
